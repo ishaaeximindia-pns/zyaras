a2519f3205005edbda0d0d74a357c23ab2b6c778
ishaaexim india <ishaaeximindia@gmail.com>
2025-12-23 13:49:18 +0000

    why there is a huge space between side bar naviagation and all products

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,4 @@
-import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
-import DashboardNav from '@/components/dashboard/DashboardNav';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      <DashboardNav />
-      <SidebarInset>{children}</SidebarInset>
-    </SidebarProvider>
-  );
+  return <>{children}</>;
 }
