26cc3fa58d1d15346f8e364bc4a310e46f4617e0
ishaaexim india <ishaaeximindia@gmail.com>
2025-12-26 01:52:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3916349809-3c802",
+  "appId": "1:395317817936:web:6bd1a8b342b33224b7d755",
+  "apiKey": "AIzaSyDBI-TTrb39B7i283xUhNspRpc8Q7_MSgM",
+  "authDomain": "studio-3916349809-3c802.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "395317817936"
+};
